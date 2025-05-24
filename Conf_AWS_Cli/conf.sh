9eba5ec0242b0722c6f8b8fc7de803c94b0bcf21
Kamilq99 <thealti222@gmail.com>
2025-05-24 17:41:48 +0200

    Some bugs fixed

--- Conf_AWS_Cli/conf.sh
+++ Conf_AWS_Cli/conf.sh
@@ -1,4 +1,3 @@
-aws configure set aws_access_key_id LocalStack
-aws configure set aws_secret_access_key LocalStack
-aws configure set default.region us-east-1
-aws configure set endpoint_url http://localhost:4566
+aws configure --profile localstack set aws_access_key_id test
+aws configure --profile localstack set aws_secret_access_key test
+aws configure --profile localstack set region us-east-1
--- S3_Bucket/provider.tf
+++ S3_Bucket/provider.tf
@@ -9,14 +9,14 @@ terraform {
 
 provider "aws" {
   region                      = "us-east-1"
-  access_key                  = "test"  # LocalStack wymaga dowolnego niepustego ciągu
-  secret_key                  = "test"  # LocalStack wymaga dowolnego niepustego ciągu
-  skip_credentials_validation = true    # Pominięcie walidacji danych AWS
-  skip_metadata_api_check     = true    # Wyłączenie sprawdzania metadanych EC2
-  skip_requesting_account_id  = true    # Pominięcie pobierania Account ID
-  s3_use_path_style           = true    # Wymagane dla LocalStack (S3 w stylu ścieżki)
+  access_key                  = "LocalStack"
+  secret_key                  = "LocalStack"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
 
   endpoints {
-    s3 = "http://localhost:4566"  # Endpoint LocalStack dla S3
+    s3 = "http://localhost:4566"
   }
 }
