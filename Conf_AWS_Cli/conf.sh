54233b386606def1ff6ced243e4d20b4ad8fd069
Kamilq99 <thealti222@gmail.com>
2025-05-24 17:04:30 +0200

    Configure AWS Cli to LocalStack

---
+++ Conf_AWS_Cli/conf.sh
@@ -0,0 +1,4 @@
+aws configure set aws_access_key_id LocalStack
+aws configure set aws_secret_access_key LocalStack
+aws configure set default.region us-east-1
+aws configure set endpoint_url http://localhost:4566
